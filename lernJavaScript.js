44e0dcc3cb3747ad15fa56c1882ee8dc4166dbbe
Hanny1989 <hans-ari.ziesemann@web.de>
2025-11-15 19:06:42 +0100

    Delete lernJavaScript.js

--- lernJavaScript.js
+++
@@ -1,8 +0,0 @@
-
-
-document.getElementById('box').innerText = 'Hello';
-
- function onclick(){
-    let onclick  = document.getElementById('but').innerHtml="fuck you";
-    console.log(onclick());
- }
